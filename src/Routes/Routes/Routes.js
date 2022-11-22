66f86bad83098eb86aba928dc4620c94e9d7a2e7
DiptaPal <diptapal1998@gmail.com>
2022-11-22 11:58:05 +0600

    doctors portal client updated

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -62,7 +62,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/dashboard/payment/:id',
-                element: <AdminRoute><Payment></Payment></AdminRoute>,
+                element: <Payment></Payment>,
                 loader: ({params}) => fetch(`https://doctors-portal-server-rosy.vercel.app/bookings/${params.id}`)
             }
         ]
